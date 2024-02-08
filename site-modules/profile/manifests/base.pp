ae5e61ce1e846bdf990189ec2c0f3dcbc8503cfa
Ken Meservy <mcswervy@fastmail.com>
2024-02-08 14:40:50 -0500

    added a test user account creation

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -2,3 +2,11 @@
 class profile::base {
   include ::accounts
 }
+
+accounts::user { 'test':
+  uid      => 9999,
+  guid     => 9999,
+  shell    => '/bin/bash',
+  password => 'test',
+  locked   => false
+}
