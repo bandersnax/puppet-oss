4e03e64b316faa36c2cc8962d7ffc6240844d372
Ken Meservy <mcswervy@fastmail.com>
2024-02-08 14:43:16 -0500

    removed test account creation, set include to require

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,12 +1,12 @@
 # The base profile should include component modules that will be on all nodes
 class profile::base {
-  include ::accounts
+  # include ::accounts
 }
 
-accounts::user { 'test':
-  uid      => 9999,
-  guid     => 9999,
-  shell    => '/bin/bash',
-  password => 'test',
-  locked   => false
-}
+# accounts::user { 'test':
+#   uid      => 9999,
+#   guid     => 9999,
+#   shell    => '/bin/bash',
+#   password => 'test',
+#   locked   => false
+# }
